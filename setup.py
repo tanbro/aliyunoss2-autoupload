db2a9a51e27b6fd3468ee70349a0ea748bd63b90
tanbro <tanbro@163.com>
2018-04-18 14:17:22 +0800

    edit: setuptools.py

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     url='https://github.com/tanbro/aliyunoss2-autoupload',
     author='liu xue yan',
     author_email='liu_xue_yan@foxmail.com',
+    license='AGPLv3+',
+    keywords='aliyun oss autoupload',
 
     use_scm_version={
         # guess-next-dev:	automatically guesses the next development version (default)
@@ -35,6 +37,8 @@ setup(
         'PyYAML',
     ],
 
+    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+
     entry_points={
         'console_scripts': [
             'aliyunoss2-autoupload = aliyunoss2_autoupload.__main__:main',
@@ -47,5 +51,33 @@ setup(
         '': ['data/*/*']
     },
 
-    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*'
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Internet',
+        'Topic :: Communications :: File Sharing',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
