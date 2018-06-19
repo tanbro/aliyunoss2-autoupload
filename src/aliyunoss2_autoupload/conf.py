806c62bffe7926ab6bbe4fd0921dd14e87f9f74e
tanbro <tanbro@163.com>
2018-06-19 14:03:17 +0800

    change: default configuration file path name

--- src/aliyunoss2_autoupload/conf.py
+++ src/aliyunoss2_autoupload/conf.py
@@ -20,10 +20,10 @@ from . import version
 
 __all__ = ['load_program_config', 'load_logging_config']
 
-DEFAULT_PROGRAM_CONFIG_PATH = 'config/prog.yml'
-ENVIRON_PROGRAM_CONFIG_PATH = '{0}_PROG_CONFIG'.format(version.NAME.upper())
+DEFAULT_PROGRAM_CONFIG_PATH = 'conf/{0}.yml'.format(version.NAME)
+ENVIRON_PROGRAM_CONFIG_PATH = '{0}_CONFIG'.format(version.NAME.upper())
 
-DEFAULT_LOGGING_CONFIG_PATH = 'config/log.yml'
+DEFAULT_LOGGING_CONFIG_PATH = 'conf/{0}.log.yml'.format(version.NAME)
 ENVIRON_LOGGING_CONFIG_PATH = '{0}_LOG_CONFIG'.format(version.NAME.upper())
 
 
